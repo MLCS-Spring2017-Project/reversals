6cc16f9a98b9f5350422135869b07acc247c6156
Amanpreet Singh <apsdehal@gmail.com>
2017-03-17 00:04:59 -0400

    PEP8

--- helpers/parse_zip.py
+++ helpers/parse_zip.py
@@ -13,31 +13,34 @@ def parse(dir, generator):
     for zfname in zipfiles:
 
         print(zfname)
-        zfile = ZipFile(zfname)    
+        zfile = ZipFile(zfname)
         year = zfname.split('/')[-1][:-4]
-        members = zfile.namelist()        
-        
+        members = zfile.namelist()
+
         threshold = len(members) / 200
-        dic = pickle.load(open("affirm_reverse.pkl", "rb"))    
-        docfreqs = []        
-        
+        dic = pickle.load(open("affirm_reverse.pkl", "rb"))
+        docfreqs = []
+
         for fname in members:
-            
+
             # "maj" means this is the majority opinion
             if not fname.endswith('-maj.txt'):
                 continue
 
             docid = fname.split('/')[-1][:-4]
 
-            if dic.loc[dic['caseid'] == docid]['Affirmed'].tolist()[0] == 0.0 && dic.loc[dic['caseid'] == docid]['Reversed'].tolist()[0] == 0.0:
+            case = dic.loc[dic['caseid'] == docid]
+
+            if case['Affirmed'].tolist()[0] == 0.0 and \
+               case['Reversed'].tolist()[0] == 0.0:
                 continue
-            elif dic.loc[dic['caseid'] == docid]['Affirmed'].tolist()[0] == 1.0:
+            elif case['Affirmed'].tolist()[0] == 1.0:
                 status = "Affirmed"
             else:
                 status = "Reversed"
 
             text = zfile.open(fname).read().decode()
-            tup = (generator(text),status)
+            tup = (generator(text), status)
             docfreqs.append(tup)
 
-        pickle.dump(docfreqs, open(zfname + ".pkl","wb"))
+        pickle.dump(docfreqs, open(zfname + ".pkl", "wb"))
