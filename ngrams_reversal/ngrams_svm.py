a7d876bb4a929e6591aee8d16ed58b3026a27c9f
Karthik Venkatesan <kv730@crunchy1.cims.nyu.edu>
2017-03-26 02:27:10 -0400

    svm attempt2

--- ngrams_reversal/ngrams_svm.py
+++ ngrams_reversal/ngrams_svm.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import os
 import pickle
+from collections import Counter
+import time
+
+
+def dotProduct(d1, d2):
+    if len(d1) < len(d2):
+        return dotProduct(d2, d1)
+    else:
+        return sum(d1.get(f, 0) * v for f, v in d2.items())
+
+
+def increment(d1, scale, d2):
+    for f, v in d2.items():
+        d1[f] = d1.get(f, 0) + v * scale
 
 
 def pegasos_fast(x, y, max_epoch, lam):
@@ -14,7 +28,7 @@ def pegasos_fast(x, y, max_epoch, lam):
     while epoch < max_epoch:
         start_time = time.time()
         epoch += 1
-        for j in xrange(x_length):
+        for j in range(x_length):
             t += 1
             eta = 1.0 / (t * lam)
             s = (1 - eta * lam) * s
@@ -52,8 +66,10 @@ def main():
     x_test = x[1500:2000]
     y_train = y_data[:1500]
     y_test = y_data[1500:2000]
-    svm_weight = pegasos_fast(x_train, y_train, 1000, 0.01)
-    print(svm_predict(x_test[0], svm_weight), y[0])
+    print('Running SVM')
+    svm_weight = pegasos_fast(x_train, y_train, 100, 0.01)
+    print('Actual value: ', y[0])
+    print('Prediction: ', svm_predict(x_test[0], svm_weight))
 
 
 if __name__ == '__main__':
