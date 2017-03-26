ea0f4c0643e8608052e6cf07765db619372d9585
Simranjyot Singh <simranjyot1991@gmail.com>
2017-03-26 01:08:46 -0400

    creating pickle of data

--- ngrams_reversal/common_cases.py
+++ ngrams_reversal/common_cases.py
@@ -3,51 +3,49 @@ import os
 import csv
 import sys
 from collections import defaultdict
+import pickle
+
 
 def create_data():
 
-	fields = ['caseid','Reversed']
-	
-	caselevel = pd.read_csv("./../../BloombergCASELEVEL_Touse.csv",skipinitialspace=True, usecols=fields)
+    fields = ['caseid', 'Reversed']
 
-	caselevel_dict = {k:v for k,v in zip(caselevel['caseid'],caselevel['Reversed'])}
-	
-	
+    caselevel = pd.read_csv("./../../BloombergCASELEVEL_Touse.csv", skipinitialspace=True, usecols=fields)
 
-	#print(caselevel_dict)
+    caselevel_dict = {k: v for k, v in zip(caselevel['caseid'], caselevel['Reversed'])}
 
-	caselevel_set = set(caselevel['caseid'])
+    caselevel_set = set(caselevel['caseid'])
 
-	path = "./../../data"
+    path = "./../../data"
 
-	dictionary = defaultdict(int)
+    dictionary = defaultdict(int)
 
-	x_data = []
+    x_data = []
 
-	y_data = []
-	
-	i=0
-	for root, dirs, files in os.walk(path):
-		#print root,dirs,files
-		for name in files:
-			
-			if name.endswith((".txt")):
-				caseid = name.replace(".txt","")
-			print(caseid)
-			if caseid in caselevel_dict:
-				i+=1
-				with open(os.path.join(root,name), mode='r') as infile:
-					reader = csv.reader(infile)
-					dictionary = {rows[0]:rows[1] for rows in reader}
+    y_data = []
 
-				#dictionary[caseid] = caselevel_dict[caseid]
-				x_data.append(dictionary)
-				y_data.append(caselevel_dict[caseid])
-				
-	print("Number of common cases between ngrams and caselevel data : ",i)
+    i = 0
+    for root, dirs, files in os.walk(path):
+        for name in files:
 
-	#print(data[0])
+            if name.endswith((".txt")):
+                caseid = name.replace(".txt", "")
+            print(caseid)
+            if caseid in caselevel_dict:
+                i += 1
+                with open(os.path.join(root, name), mode='r') as infile:
+                    reader = csv.reader(infile)
+                    dictionary = {rows[0]: rows[1] for rows in reader}
+
+                x_data.append(dictionary)
+                y_data.append(caselevel_dict[caseid])
+
+    print("Number of common cases between ngrams and caselevel data : ", i)
+    with open('./../../x_data.pickle', 'wb') as handle:
+        pickle.dump(x_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('./../../y_data.pickle', 'wb') as handle:
+        pickle.dump(y_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-	create_data()
+    create_data()
