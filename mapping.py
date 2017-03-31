c2460c4f2e17341263b88a5bb997faf79b56684f
Amanpreet Singh <apsdehal@gmail.com>
2017-03-31 16:14:58 -0400

    Restructure mapping.py as more modular

--- mapping.py
+++ mapping.py
@@ -10,35 +10,43 @@ from difflib import SequenceMatcher
 from helpers.circuit_parser import CircuitParser
 
 
-link = dict()
-similarity_score = 0.9
+class Mapper:
+    def __init__(self):
+        self.link = dict()
+        self.similarity_score = 0.9
 
-with open(sys.argv[1], newline='') as file:
+        with open(sys.argv[1], newline='') as file:
 
-    entry = csv.reader(file)
+            self.entry = csv.reader(file)
 
-    for row in entry:
+            for row in self.entry:
 
-        if row[1] not in link:
-            link[row[1]] = []
+                if row[1] not in self.link:
+                    self.link[row[1]] = []
 
-        if row[0] not in link[row[1]]:
-            link[row[1]].append(row[0])
+                if row[0] not in self.link[row[1]]:
+                    self.link[row[1]].append(row[0])
 
-# print(link)
+    def run(self):
+        self.found = dict()
+        self.parser = CircuitParser()
 
-found = dict()
-count = 0
+        with open(sys.argv[3], newline='') as csv_file:
+            self.csv_file = csv_file
 
-with open(sys.argv[3], newline='') as csv_file:
+            self.entry = csv.reader(self.csv_file)
+            os.chdir(sys.argv[2])
+            zipfiles = glob('*zip')
 
-    entry = csv.reader(csv_file)
-    os.chdir(sys.argv[2])
-    zipfiles = glob('*zip')
+            for zfname in zipfiles:
 
-    for zfname in zipfiles:
+                print(zfname)
+                self.link_year(zfname)
 
-        print(zfname)
+        with open('circuit_to_district_link_0.7.pickle', 'wb') as handle:
+            pickle.dump(self.found, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def link_year(self, zfname):
         year = int(zfname.split('/')[-1][:-4])
 
         # if year < 1927:
@@ -46,9 +54,6 @@ with open(sys.argv[3], newline='') as csv_file:
 
         zfile = ZipFile(zfname)
         members = zfile.namelist()
-
-        parser = CircuitParser()
-        count += 1
 
         for fname in members:
 
@@ -59,36 +64,38 @@ with open(sys.argv[3], newline='') as csv_file:
             text_pre = zfile.open(fname).read().decode('utf-8')
 
             try:
-                case_det = parser.parse(text_pre)
+                case_det = self.parser.parse(text_pre)
             except Exception as e:
                 continue
 
-            if len(case_det.keys()) == 0 or 'case_name' not in case_det or 'circuit_number' not in case_det:
+            if len(case_det.keys()) == 0 or 'case_name' not in case_det or \
+               'circuit_number' not in case_det:
                 continue
 
             case_det["circuit_number"] = str(case_det["circuit_number"])
-            csv_file.seek(0)
-            next(entry, None)
+            self.csv_file.seek(0)
+            next(self.entry, None)
 
-            for row in entry:
+            for row in self.entry:
 
                 date = row[4].split("/", 2)
                 date[2] = int(date[2])
-                row[1] = parser.format(row[1])
+                row[1] = self.parser.format(row[1])
 
                 if date[2] <= year:
 
                     if abs(year - date[2]) <= 5:
-                        if case_det["circuit_number"] in link:
-                            # print(link[case_det["circuit_number"]], row[2])
-                            if row[2] in link[case_det["circuit_number"]]:
+                        if case_det["circuit_number"] in self.link:
+                            if row[2] in self.link[case_det["circuit_number"]]:
 
-                                if SequenceMatcher(None, case_det["case_name"], row[1]).ratio() > similarity_score:
+                                if SequenceMatcher(None, case_det["case_name"],
+                                   row[1]).ratio() > self.similarity_score:
 
-                                    if docid not in found:
-                                        found[docid] = []
+                                    if docid not in self.found:
+                                        self.found[docid] = []
 
-                                    found[docid].append(row[0] + "/" + row[2])
+                                    self.found[docid].append(row[0] + "/" +
+                                                             row[2])
                                     print(docid, row[0], row[2], "Link")
 
                                 else:
@@ -99,12 +106,15 @@ with open(sys.argv[3], newline='') as csv_file:
 
                             if row[3].find(case_det["circuit_number"]) != -1:
 
-                                if SequenceMatcher(None, case_det["case_name"], row[1]).ratio() > similarity_score:
+                                if SequenceMatcher(None, case_det["case_name"],
+                                                   row[1]).ratio() > \
+                                   self.similarity_score:
 
-                                    if docid not in found:
-                                        found[docid] = []
+                                    if docid not in self.found:
+                                        self.found[docid] = []
 
-                                    found[docid].append(row[0] + "/" + row[2])
+                                    self.found[docid].append(row[0] + "/" +
+                                                             row[2])
                                     print(docid, row[0])
 
                                 else:
@@ -117,5 +127,6 @@ with open(sys.argv[3], newline='') as csv_file:
                     break
 
 
-with open('circuit_to_district_link_0.7.pickle', 'wb') as handle:
-    pickle.dump(found, handle, protocol=pickle.HIGHEST_PROTOCOL)
+if __name__ == '__main__':
+    mapper = Mapper()
+    mapper.run()
