39458f36a1c7a46875d386c5a8a414e68941d8eb
Amanpreet Singh <apsdehal@gmail.com>
2017-04-25 20:13:44 -0400

    Fix unpublished pickle converter

--- scripts/unpublished_pickle_converter.py
+++ scripts/unpublished_pickle_converter.py
@@ -18,6 +18,8 @@ def main():
     for item, value in affirm_reverse_dict.items():
         if value[0] not in pickles:
             f = "pickles/%s_95.pkl" % value[0]
+            if not os.path.exists(f):
+                continue
             dic = pickle.load(open(f, "rb"))
             pickles[value[0]] = dic
         else:
@@ -27,13 +29,13 @@ def main():
         # splitting the district caseid from the folder in which it would be found
         path = value[1][0].split("/")
         # saving the data in new dataframe
-        affirmed = value[3] == "Affirmed"
-        reversed = value[3] == "Reversed"
-        df.loc[count] = [path[0], path[1], str(year), afffirmed, 0, 0, reversed]
+        affirmed = value[2] == "Affirmed"
+        reversed = value[2] == "Reversed"
+        df.loc[count] = [path[0], path[1], str(year), int(affirmed), 0, 0, int(reversed)]
 
         count += 1
 
-    with open('unique_district_affirm_reverse.pkl', 'wb') as handle:
+    with open('unpublished_district_affirm_reverse.pkl', 'wb') as handle:
             pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
