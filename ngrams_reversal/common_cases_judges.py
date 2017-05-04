8328dfc5951823f91acd3802d77805e7e66395b3
Simranjyot Singh <simranjyot1991@gmail.com>
2017-05-03 23:58:45 -0400

    rerading with pickle instead of csv

--- ngrams_reversal/common_cases_judges.py
+++ ngrams_reversal/common_cases_judges.py
@@ -11,25 +11,34 @@ def count_judges(j1_party, j2_party, j3_party):
     num_judges_party2 = 0
     num_judges_party_other = 0
 
-    if int(j1_party) == 0:
-        num_judges_party1 += 1
-    elif int(j1_party) == 1:
-        num_judges_party2 += 1
-    else:
+    try:
+        if int(j1_party) == 0:
+            num_judges_party1 += 1
+        elif int(j1_party) == 1:
+            num_judges_party2 += 1
+        else:
+            num_judges_party_other += 1
+    except ValueError:
         num_judges_party_other += 1
 
-    if int(j2_party) == 0:
-        num_judges_party1 += 1
-    elif int(j2_party) == 1:
-        num_judges_party2 += 1
-    else:
+    try:
+        if int(j2_party) == 0:
+            num_judges_party1 += 1
+        elif int(j2_party) == 1:
+            num_judges_party2 += 1
+        else:
+            num_judges_party_other += 1
+    except ValueError:
         num_judges_party_other += 1
 
-    if int(j3_party) == 0:
-        num_judges_party1 += 1
-    elif int(j3_party) == 1:
-        num_judges_party2 += 1
-    else:
+    try:
+        if int(j3_party) == 0:
+            num_judges_party1 += 1
+        elif int(j3_party) == 1:
+            num_judges_party2 += 1
+        else:
+            num_judges_party_other += 1
+    except ValueError:
         num_judges_party_other += 1
 
     return num_judges_party1, num_judges_party2, num_judges_party_other
@@ -46,7 +55,7 @@ def create_data():
 
     for case_id, j1_party, j2_party, j3_party in zip(caselevel['caseid'], caselevel['j1party'], caselevel['j2party'], caselevel['j3party']):
         num_judges_party1, num_judges_party2, num_judges_party_other = count_judges(j1_party, j2_party, j3_party)
-        caselevel_dict_judges[caseid] = (num_judges_party1, num_judges_party2, num_judges_party_other)
+        caselevel_dict_judges[case_id] = (num_judges_party1, num_judges_party2, num_judges_party_other)
 
     # caselevel_set = set(caselevel['blcaseid'])
 
@@ -67,20 +76,25 @@ def create_data():
             print(caseid)
             if caseid in caselevel_dict:
                 i += 1
-                with open(os.path.join(root, name), mode='r') as infile:
-                    reader = csv.reader(infile)
-                    dictionary = {rows[0]: int(rows[1]) for rows in reader}
-                    dictionary['num_judges_party1'] = num_judges_party1
-                    dictionary['num_judges_party2'] = num_judges_party2
-                    dictionary['num_judges_party_other'] = num_judges_party_other
+                with open(os.path.join(root, name), mode='rb') as infile:
+                    dictionary = pickle.load(infile)
+
+                num_judges_party1, num_judges_party2, num_judges_party_other = caselevel_dict_judges[caseid]
+                dictionary['num_judges_party1'] = num_judges_party1
+                dictionary['num_judges_party2'] = num_judges_party2
+                dictionary['num_judges_party_other'] = num_judges_party_other
 
                 x_data.append(dictionary)
                 y_data.append(caselevel_dict[caseid])
+                # print(dictionary)
+                # print(caseid)
+                # print('############')
+                # input('wait')
 
     print("Number of common cases between ngrams and caselevel data : ", i)
-    with open('./../../x_data.pickle', 'wb') as handle:
+    with open('./../../x_data_judges.pickle', 'wb') as handle:
         pickle.dump(x_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open('./../../y_data.pickle', 'wb') as handle:
+    with open('./../../y_data_judges.pickle', 'wb') as handle:
         pickle.dump(y_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
