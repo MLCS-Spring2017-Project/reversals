0b011a27ccb897d01f36ae48dc7ff9c45fe1b988
Amanpreet Singh <apsdehal@gmail.com>
2017-05-07 17:04:32 -0400

    Add dnn trainer

---
+++ dnn/train.py
@@ -0,0 +1,21 @@
+from magpie import MagpieModel
+import os
+
+
+class Trainer:
+    def __init__(self, config):
+        self.config = config
+        self.magpie = MagpieModel()
+
+    def train(self):
+        self.magpie.init_word_vectors(self.config["train_path"], vec_dim=200)
+        self.labels = ["Affirmed", "Reversed"]
+        self.magpie.batch_train(self.config["train_path"], labels, test_dir=self.config["test_path"], nb_epochs=30)
+
+    def save(self):
+        embeddings_path = os.path.join(self.config["save_path"], "embeddings", "embeddings")
+        scalar_path = os.path.join(self.config["save_path"], "scalar", "scalar")
+        model_path = os.path.join(self.config["save_path"], "model", "model.h5")
+        self.magpie.save_word2vec_model(embeddings_path, overwrite=True)
+        self.magpie.save_scaler(scalar_path, overwrite=True)
+        self.magpie.save_model(model_path, overwrite=True)
