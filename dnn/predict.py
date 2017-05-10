8c63b022112c023e3009e9f46f8351ac9fca3dba
Amanpreet Singh <apsdehal@gmail.com>
2017-05-10 13:04:46 -0400

    Fix prediction dnn script

--- dnn/predict.py
+++ dnn/predict.py
@@ -1,42 +1,49 @@
 from magpie import MagpieModel
 from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from glob import glob
+import logging
+import os
+
+log = logging.getLogger(__name__)
 
 
 class Predict:
     def __init__(self, config):
         self.config = config
         embeddings_path = os.path.join(self.config["save_path"], "embeddings", "embeddings")
-        scalar_path = os.path.join(self.config["save_path"], "scalar", "scalar")
+        scaler_path = os.path.join(self.config["save_path"], "scaler", "scaler")
         model_path = os.path.join(self.config["save_path"], "model", "model.h5")
 
-        if os.path.exists(embeddings_path) and os.path.exists(scalar_path) and os.path.exists(model_path):
+        if os.path.exists(embeddings_path) and os.path.exists(scaler_path) and os.path.exists(model_path):
             self.magpie = MagpieModel(
                 keras_model=model_path,
                 word2vec_model=embeddings_path,
-                scaler=scalar_path,
+                scaler=scaler_path,
                 labels=['Affirmed', 'Reversed']
             )
         else:
-            raise Exception("Model, scalar and embedded must be generated before predicting")
+            raise Exception("Model, scaler and embedded must be generated before predicting")
 
     def predict(self):
-        curr_dir = os.getcwd()
-        os.chdir(config["test_path"])
-
-        files = glob("./**/*.txt")
+        test_path = os.path.join(self.config['test_path'], "**", "*.txt")
+        files = glob(test_path, recursive=True)
         y = []
         predicted = []
         for fname in files:
-            lab = fname[-4] + ".lab"
+            lab = fname[:-4] + ".lab"
             with open(lab, "r") as f:
                 status = f.read()
 
-            prediction = self.magpie.predict_from_file(fname)[0]
+            prediction = self.magpie.predict_from_file(fname)[0][0]
             predicted.append(prediction)
             y.append(status)
 
-        os.chdir(curr_dir)
-        fpr, tpr, thresholds = metrics.roc_curve(y, predicted_status)
+        print(predicted, y)
+        encoder = LabelEncoder()
+        y = encoder.fit_transform(y)
+        predicted = encoder.transform(predicted)
+        fpr, tpr, thresholds = metrics.roc_curve(y, predicted)
 
-        print(metrics.auc(fpr, tpr))
-        print(metrics.confusion_matrix(y, predicted_status))
+        log.info(metrics.auc(fpr, tpr))
+        log.info(metrics.confusion_matrix(y, predicted))
