95a4242104d8465465d427e5541e4f9e0449cc71
Amanpreet Singh <apsdehal@gmail.com>
2017-05-10 13:05:02 -0400

    Use trained word2vec models and scaler

--- dnn/train.py
+++ dnn/train.py
@@ -1,25 +1,43 @@
 from magpie import MagpieModel
 import os
+import logging
+log = logging.getLogger(__name__)
 
 
 class Trainer:
     def __init__(self, config):
         self.config = config
-        self.magpie = MagpieModel()
+        embeddings_path = os.path.join(self.config["save_path"], "embeddings", "embeddings")
+        scaler_path = os.path.join(self.config["save_path"], "scaler", "scaler")
+
+        if os.path.exists(embeddings_path):
+            self.magpie = MagpieModel(word2vec_model=embeddings_path)
+        else:
+            self.magpie = MagpieModel()
+
+        if os.path.exists(scaler_path):
+            self.magpie.load_scaler(scaler_path)
 
     def train(self):
-        print("Init work vectors, start")
-        self.magpie.init_word_vectors(self.config["train_path"], vec_dim=200)
-        print("Init work vectors, done")
+        if not self.magpie.word2vec_model:
+            log.info("Init work vectors, start")
+            self.magpie.init_vectors(self.config["train_path"])
+            log.info("Init work vectors, done")
+
+        if not self.magpie.scaler:
+            log.info("Starting scaler")
+            self.magpie.fit_scaler(self.config["train_path"])
+            log.info("Starting scaler")
+
         self.labels = ["Affirmed", "Reversed"]
-        print("Starting train")
-        self.magpie.batch_train(self.config["train_path"], labels, test_dir=self.config["test_path"], nb_epochs=1)
-        print("Trained")
+        log.info("Starting training")
+        self.magpie.batch_train(self.config["train_path"], self.labels, test_dir=self.config["test_path"], nb_epochs=30)
+        log.info("Trained")
 
     def save(self):
         embeddings_path = os.path.join(self.config["save_path"], "embeddings", "embeddings")
-        scalar_path = os.path.join(self.config["save_path"], "scalar", "scalar")
+        scaler_path = os.path.join(self.config["save_path"], "scaler", "scaler")
         model_path = os.path.join(self.config["save_path"], "model", "model.h5")
-        self.magpie.save_word2vec_model(embeddings_path, overwrite=True)
-        self.magpie.save_scaler(scalar_path, overwrite=True)
-        self.magpie.save_model(model_path, overwrite=True)
+        # self.magpie.save_word2vec_model(embeddings_path, overwrite=True)
+        self.magpie.save_scaler(scaler_path, overwrite=True)
+        self.magpie.save_model(model_path)
